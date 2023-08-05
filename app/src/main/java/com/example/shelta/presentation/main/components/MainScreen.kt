672ceb193b4d7774d998ca17407ba509adf0292c
b3njah <benjahjp@gmail.com>
2023-08-05 13:44:05 +0300

    feat: image picker

--- app/src/main/java/com/example/shelta/presentation/main/components/MainScreen.kt
+++ app/src/main/java/com/example/shelta/presentation/main/components/MainScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.rounded.Upload
@@ -36,6 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -138,7 +140,7 @@ fun MainScreen(
                         Card(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .height(500.dp)
+                                .height(700.dp)
                                 .padding(20.dp)
                             ,
                         ) {
@@ -158,6 +160,7 @@ fun MainScreen(
                             AsyncImage(
                                 modifier = Modifier
                                     .padding(16.dp)
+                                    .clip(RoundedCornerShape(16.dp))
                                 ,
                                 model = selectedImageUri,
                                 contentScale = ContentScale.Crop,
